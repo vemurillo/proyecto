c172d642a1194de00d1a611dab4e89b9c0b6e80e
Victor <victorwwe350@hotmail.com>
2019-07-03 22:07:58 -0600

    Se agrega la autentificacion con firebase faltando el cerrar session

--- app/src/main/java/com/example/jurescaym/MainActivity.java
+++ app/src/main/java/com/example/jurescaym/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.jurescaym;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Este es el link para utilizar los servicios de autenticación.
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicializa la comunicación el servicio de autenticación
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Valida si el usuario se mantiente autenticado...
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user){
+        if (user!=null) {
+            Intent intent = new Intent(this, MenuPrincipal.class);
+            startActivity(intent);
+        }
+    }
+
+    public void registrarse(View view){
+        String email = ((EditText)findViewById(R.id.etCorreo)).getText().toString();
+        String password = ((EditText)findViewById(R.id.etContrasenna)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //si se registra... entonces pasará la info a la otra pantalla
+                            Log.d("Creando", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // Si da error la creación... se mantiene en esta pantalla
+                            Log.w("Error", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+
+    public void ingresar(View view) {
+        String email = ((EditText)findViewById(R.id.etCorreo)).getText().toString();
+        String password = ((EditText)findViewById(R.id.etContrasenna)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //La autenticación se dió... se pasa a la pantalla principal
+                            Log.d("Autenticando", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            //Error de autenticación
+                            Log.w("error", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
